eb30b4b046c171094cc0f9d95d81563a677b043c
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-13 23:08:55 +0530

    [Suraj] Add:Inventory Managemnet

---
+++ OopsProblem/OopsProblem/Inventory Management/InventoryDetailsManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OopsProblem.Inventory_Management
+{
+    internal class InventoryDetailsManagement
+    {
+        public void ReadJsonFile(string file)
+        {
+            var JsonData = File.ReadAllText(file);
+            List<Inventory> inventories = JsonConvert.DeserializeObject<List<Inventory>>(JsonData);
+            foreach (var data in inventories)
+            {
+                Console.WriteLine(data.Name + "\n" + data.Price + "\n" + data.Weight);
+            }
+        }
+    }
+}
